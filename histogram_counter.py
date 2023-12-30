c82e2c106f2304149cf256b09962cdbba9ee8623
Adam Benda <adam@adasek.cz>
2023-12-30 11:40:06 +0100

    Histogram with unique and total counts

--- histogram_counter.py
+++ histogram_counter.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter, StrMethodFormatter
 import numpy as np
 
-coprus_words_counter = load_counter_pickle("corpus/word_counter2.pickle")
+coprus_words_counter = load_counter_pickle("corpus/word_counter3.pickle")
 
 counts = list(coprus_words_counter.values())
 
@@ -11,7 +11,7 @@ print(f"counts {len(counts)}")
 
 print(min(counts))
 print(max(counts))
-bins = list(range(10)) + [round(x) for x in np.logspace(1, np.ceil(np.log10(max(counts))), num=50)]
+bins = sorted(list(set([round(x) for x in np.logspace(0, np.ceil(np.log10(max(counts))), num=200)])))
 # bins = [0, 1, 2, 3, 10, 100, 1000, 10000]
 print(bins)
 print(f"np.arrange {len(counts)}")
@@ -19,30 +19,32 @@ print(f"np.arrange {len(counts)}")
 plt.xscale('log')
 plt.yscale('log')
 
-plt.gca().xaxis.set_major_formatter(StrMethodFormatter("{x:.0f}"))
-plt.gca().yaxis.set_major_formatter(StrMethodFormatter("{x:.0f}"))
+plt.xlabel('Word Frequency: Rare to Common')
+plt.ylabel('Unique Words Count')
 
-# fig1, ax1 = plt.subplots()
-# ax2 = ax1.twinx()
-# ax1.ticklabel_format(axis="x", style="plain")
-# ax2.ticklabel_format(axis="y", style="plain")
-
-# plt.gca().xaxis.ticklabel_format = 'plain'
-# plt.gca().yaxis.ticklabel_format = 'plain'
-
-# Creating a histogram using Matplotlib
-plt.hist(counts, bins=bins, color='blue')
+# plt.gca().xaxis.set_major_formatter(StrMethodFormatter("{x:.0f}"))
+# plt.gca().yaxis.set_major_formatter(StrMethodFormatter("{x:.0f}"))
 
 
-print("bar")
+hist, bin_edges, _ = plt.hist(counts, bins=bins, color='blue')
+
+ax2 = plt.gca().twinx()
+
+# cumulative_sum = np.cumsum(hist * bin_edges[:-1])
+multiplied_values = hist * bin_edges[:-1]
+
+ax2.plot(bin_edges[:-1], multiplied_values, color='red', marker='o', linestyle='-', linewidth=2, markersize=4, label='Words')
+ax2.set_ylabel('Total Words Count', color='red')
+ax2.tick_params(axis='y', labelcolor='red')
+ax2.set_yscale('log')
+# ax2.yaxis.set_major_formatter(StrMethodFormatter("{x:.0f}"))
+
 
 # Adding labels and title
-plt.xlabel('Words')
-plt.ylabel('Counts')
 plt.title('Histogram of Words Counter')
 
 # Saving the plot as a PNG file
-plt.savefig('histogram.png')
+plt.savefig('results/histogram_word_counter3.png')
 
 # Displaying the plot
 plt.show()
